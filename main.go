801c7fdcabf5653288e8d2ed0eb2e6c1635caf25
shialongsworth <2020151519@ub.edu.bz>
2023-02-15 12:38:34 -0600

    flag branch

--- main.go
+++ main.go
@@ -11,9 +11,11 @@ func main() {
 	//set the flags   name of flag, default value of flag, if u dont know how to use the flag
 	msg := flag.String("msg", "howdy, stranger!", "the message to display")
 	num := flag.Int("num", 1, "how many times to print the message")
+	//caps := flag.Bool("caps", false, "should the string be all caps")
 	flag.Parse()
 
 	for i := 0; i < *num; i++ {
 		fmt.Println(*msg)
 	}
+
 }
